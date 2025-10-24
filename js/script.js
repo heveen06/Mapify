cce5802462e5c586abfd1c44aa2cbba197aa3b3c
heveen06 <heveen.almhd7@gmail.com>
2025-10-24 22:27:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const countries = document.querySelectorAll("path");
+// const colors = ["color1", "color2", "color3", "color4", "color5"];
+
+// countries.forEach((country, i) => {
+//   country.classList.add(colors[i % colors.length]);
+// });
